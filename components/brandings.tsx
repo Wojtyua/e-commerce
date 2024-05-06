78d79f71a98bd6c80e3141d2fda2b057f1d7bfac
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-05-06 17:16:16 +0200

    adjusted logos size

--- components/brandings.tsx
+++ components/brandings.tsx
@@ -38,7 +38,7 @@ const Brandings = () => {
       <ul className="grid place-items-center grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8 mx-auto w-full items-center">
         {brands.map((brand) => (
           <li key={brand.name} className="p-4">
-            <Image src={brand.logo} alt={brand.name} width={150} height={150} />
+            <Image src={brand.logo} alt={brand.name} width={125} height={125} />
           </li>
         ))}
       </ul>
