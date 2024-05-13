932dfb3ed2c4ed7e2069e56f78753d20300d0aa9
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-05-13 16:52:59 +0200

    hero fix

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,15 +3,15 @@ import { Button } from "./ui/button";
 const HeroSection = () => {
   return (
     <section className="grid grid-row-2 md:grid-cols-2 items-center bg-landing  w-full h-[750px] bg-cover bg-center bg-no-repeat filter grayscale-[.45] relative">
-      <div className="absolute right-0 bg-black bg-opacity-70 p-5 text-gray-50">
-        <h1 className="text-4xl font-semibold text-white">
+      <div className="absolute right-0 bg-black bg-opacity-70 p-5 text-white-primary flex flex-col gap-4">
+        <h1 className="text-4xl font-semibold ">
           The best sneakers in the world
         </h1>
-        <p className="text-white mt-4">
+        <p>
           We have the best sneakers in the world. Lorem ipsum dolor sit amet,
           consectetur adipisicing elit. Quisquam, quod.
         </p>
-        <Button className="mt-4">Shop now</Button>
+        <Button className="w-28">Shop now</Button>
       </div>
     </section>
   );
