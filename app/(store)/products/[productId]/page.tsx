ddffcce2b3dc55ac9fd6d0260e062245d96bed4a
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-05-22 15:14:53 +0200

    refactored files structure

--- app/(store)/products/[productId]/page.tsx
+++ app/(store)/products/[productId]/page.tsx
@@ -4,11 +4,11 @@ import { getProductById, FormattedProduct } from "@/utils/getProductById";
 import { useQuery } from "@tanstack/react-query";
 import useCartStore from "@/lib/store";
 
-import ProductPending from "@/components/ui/ProductPending";
-import ImageGallery from "@/components/ImageGallery";
 import ProductHeader from "../_components/ProductHeader";
 import AddToCartButton from "../../_components/ui/AddToCartButton";
 import AddToWishlistButton from "../../_components/ui/AddtoWishlistButton";
+import ProductPending from "../_components/ProductPending";
+import ImageGallery from "../_components/ImageGallery";
 
 const ProductDetailsPage = ({ params }: { params: { productId: number } }) => {
   const productId = params.productId;
