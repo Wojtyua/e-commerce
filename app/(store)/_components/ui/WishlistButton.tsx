86f5fcf16b9716937f7a992e5e672b219146fc6c
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-05-24 14:12:04 +0200

    fixed wishlist button

--- app/(store)/_components/ui/WishlistButton.tsx
+++ app/(store)/_components/ui/WishlistButton.tsx
@@ -1,3 +1,4 @@
+import useCartStore from "@/lib/store";
 import { CiHeart } from "react-icons/ci";
 import {
   Drawer,
@@ -11,10 +12,21 @@ import {
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
 
+import Image from "next/image";
+import Link from "next/link";
+
 const WishlistButton = () => {
+  const cart = useCartStore((state) => state.items);
   return (
     <Drawer>
-      <DrawerTrigger>click</DrawerTrigger>
+      <DrawerTrigger asChild>
+        <button className="relative hover:bg-neutral-200 transition rounded-full p-2">
+          <CiHeart size={25} />
+          <span className="absolute bg-black-primary text-white-primary flex items-center justify-center text-xs font-bold rounded-full bottom-1 right-1 size-4">
+            {cart.length}
+          </span>
+        </button>
+      </DrawerTrigger>
       <DrawerContent>
         <div className="mx-auto w-full max-w-4xl">
           <DrawerHeader className="border-b">
@@ -26,12 +38,33 @@ const WishlistButton = () => {
             <DrawerDescription>Check out your wishlist</DrawerDescription>
           </DrawerHeader>
           <div className="p-4 pb-0">
-            <div className="flex items-center justify-center space-x-2"></div>
-            <div className="mt-3 h-[120px]"></div>
+            <div className="flex flex-col gap-4">
+              {cart.map((item) => (
+                <div key={item.id} className="flex gap-2">
+                  <Image
+                    src={item.imageUrl || "not found"}
+                    alt={item.model || "not found"}
+                    width={48}
+                    height={48}
+                    className="w-12 h-12 object-cover rounded-lg"
+                  />
+                  <div>
+                    <Link href={`/products/${item.id}`}>
+                      <p className="font-semibold capitalize underline cursor-pointer">
+                        {item.model}
+                      </p>
+                    </Link>
+                    <p>{item.price} PLN</p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
           <DrawerFooter>
             <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
+              <Button className="w-full" variant="outline">
+                Cancel
+              </Button>
             </DrawerClose>
           </DrawerFooter>
         </div>
