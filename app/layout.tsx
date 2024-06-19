d5a8692b681613fd3f53fa78f3d449d68e771283
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-19 21:21:29 +0200

    chore: Remove ReactQueryProvider and update layout in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 
 import "./globals.css";
-import ReactQueryProvider from "@/providers/reactQueryProvider";
 import Navbar from "./(store)/_components/navbar/Navbar";
 import Footer from "./(store)/_components/Footer";
+import QueryProvider from "@/lib/QueryProvider";
 
 // POEKSPERYMENTUJ PÓŹNIEJ ZE ZMIANĄ NA INNY FONT
 const font = Urbanist({ subsets: ["latin"] });
@@ -17,19 +17,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
         className={`${font.className} antialiased overflow-y-scroll flex flex-col min-h-screen`}
       >
-        <Navbar />
-        <main className="text-primary flex-1">
-          <ReactQueryProvider>{children}</ReactQueryProvider>
-        </main>
-        <Footer />
+        <QueryProvider>
+          <Navbar />
+          <main className="text-primary flex-1">{children}</main>
+          <Footer />
+        </QueryProvider>
       </body>
     </html>
   );
--- lib/QueryProvider.tsx
+++ lib/QueryProvider.tsx
@@ -1,17 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
 
-const ReactQueryProvider = ({ children }: React.PropsWithChildren) => {
-  const [client] = useState(() => new QueryClient());
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
 
-export default ReactQueryProvider;
+export default QueryProvider;
