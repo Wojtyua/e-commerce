8a020ec05c5c41119306992941ffced117eab5ee
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-24 18:55:54 +0200

    basic card for products

--- app/(store)/(target_group)/men/page.tsx
+++ app/(store)/(target_group)/men/page.tsx
@@ -2,6 +2,7 @@
 
 import { getMensProducts } from "@/utils/getMenProducts";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 
 const MenPage = () => {
   const { data, status } = useQuery({
@@ -9,7 +10,24 @@ const MenPage = () => {
     queryFn: getMensProducts,
     staleTime: 1000 * 60 * 5,
   });
-  return <div>Men page</div>;
+  if (status === "pending") return <div>Loading...</div>;
+  return (
+    <div className="grid grid-cols-3 gap-3 p-5">
+      {data?.map((product) => (
+        <div key={product.id}>
+          <Image
+            src={product.image_url || "placeholder"}
+            alt={product.model || "placeholder"}
+            width={300}
+            height={300}
+            className="w-auto h-auto"
+          />
+          <h2>{product.model}</h2>
+          <p>{product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MenPage;
