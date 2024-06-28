eef0e61dc9d3f567a16919c7a4a39258186535a3
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-06-28 09:46:38 +0200

    added signup and login page

---
+++ app/(auth)/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "./_components/LoginForm";
+
+const Page = () => {
+  return (
+    <div className="flex h-svh items-center">
+      <LoginForm />;
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/(auth)/auth/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SignUpForm } from "./_components/SignUpForm";
+
+const Page = () => {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Page;
