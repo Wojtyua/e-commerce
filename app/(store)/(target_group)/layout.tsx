1bea8a66717a845004185789175e1a76481bb153
Wojtyua <wojciechderlikiewicz@gmail.com>
2024-08-20 21:03:30 +0200

    changed: filter aside bar

--- app/(store)/(target_group)/layout.tsx
+++ app/(store)/(target_group)/layout.tsx
@@ -1,8 +1,8 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-10">
-      <aside className="hidden md:block col-span-1 border-r">filter</aside>
-      <main className="col-span-10 md:col-span-9 mx-auto">{children}</main>
+    <div className="grid grid-cols-12">
+      <aside className="hidden md:block col-span-2 border-r">filter</aside>
+      <main className="col-span-10 md:col-span-10 mx-auto">{children}</main>
     </div>
   );
 };
